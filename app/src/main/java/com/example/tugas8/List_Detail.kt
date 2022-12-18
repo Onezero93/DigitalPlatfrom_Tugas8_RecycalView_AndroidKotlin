315a35448bc99fdbcf00cd3057d8b35721ea32b2
iqbal suwandi <qbalsuwandi@gmail.com>
2022-12-19 01:28:20 +0700

    10.memanggil item list di List_Detail.kt supaya tampil di detail layoutnya

--- app/src/main/java/com/example/tugas8/List_Detail.kt
+++ app/src/main/java/com/example/tugas8/List_Detail.kt
@@ -2,10 +2,25 @@ package com.example.tugas8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_list_detail.*
 
 class List_Detail : AppCompatActivity() {
+    @Suppress("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_detail)
+        val titel = findViewById<TextView>(R.id.tv_titel)
+        val deskripsi = findViewById<TextView>(R.id.tv_desc)
+
+
+        val imag = intent.getIntExtra("imag",0)
+        val titel1 = intent.getStringExtra("nama")
+        val deskripsi1 = intent.getStringExtra("desc")
+
+        img_list.setImageResource(imag)
+        titel.text = titel1
+        deskripsi.text = deskripsi1
+
     }
 }
